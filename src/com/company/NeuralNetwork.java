201feb963d63f8a82596e7881ad0b419aaf9f20b
darwin <seilov.skynet@gmail.com>
2019-03-08 16:15:52 +0600

    init

---
+++ src/com/company/NeuralNetwork.java
@@ -0,0 +1,111 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class NeuralNetwork {
+
+        private double lr;
+        private double[][] wih;
+        private double[][] who;
+
+        public NeuralNetwork(int inodes, int hnodes, int onodes, double lr) {   //inodes - input nodes, hnodes - hidden nodes, onodes - output nodes, lr - learning rate 0 > lr < 1
+
+                if(inodes != hnodes || hnodes != onodes || lr == 0)
+                        throw new RuntimeException("invalid input params: inodes = " + inodes + " hnodes = " + hnodes + " onodes = " + onodes + " lr = " + lr);
+                this.lr = lr;
+                this.wih = new double[hnodes][inodes];
+                this.who = new double[onodes][hnodes];
+
+                for (int i = 0; i < hnodes; i++) {      //init weights
+                        for (int k = 0; k < inodes; k++) {
+                                wih[i][k] = Math.random();
+                        }
+                }
+
+                for (int i = 0; i < onodes; i++) {      //init weights
+                        for (int k = 0; k < hnodes; k++) {
+                                who[i][k] = Math.random();
+                        }
+                }
+        }
+
+        public static void main(String[] args) {
+
+
+                double[] data = {1.0, 0.0, 1.0};        //training data
+                double[] data2 = {0.0, 1.0, 1.0};
+                double[] target = {1.0, 1.0, 0.0};
+                double[] target2 = {0.0, 1.0, 1.0};
+
+                NeuralNetwork neuralNetwork = new NeuralNetwork(3, 3, 3, 0.3);
+
+
+                for (int i = 0; i < 1000; i++) {        //training
+                        neuralNetwork.train(data, target);
+                        neuralNetwork.train(data2, target2);
+
+                }
+
+
+                double[] res = neuralNetwork.test(data);        //test 1
+                System.out.println("\ntarget = " + Arrays.toString(target));
+                System.out.println("res = " + Arrays.toString(res));
+                System.out.println("err = " + Arrays.toString(Utils.minusMatrix(target, res)));
+
+                double[] res2 = neuralNetwork.test(data2);      //test 2
+                System.out.println("\ntarget2 = " + Arrays.toString(target2));
+                System.out.println("res = " + Arrays.toString(res2));
+                System.out.println("err = " + Arrays.toString(Utils.minusMatrix(target2, res2)));
+
+
+        }
+
+
+        private double[] test(double[] inputs) {        //test method
+
+                double[] hiddenInputs = Utils.multiplyMatrix(this.wih, inputs);
+
+                double[] hiddenOutputs = Arrays.stream(hiddenInputs).map(Utils::activationFunction).toArray();
+
+                double[] finalInputs = Utils.multiplyMatrix(this.who, hiddenOutputs);
+
+                return Arrays.stream(finalInputs).map(Utils::activationFunction).toArray();
+        }
+
+        private void train(double[] inputs, double[] target) {  //training method
+
+                double[] hiddenInputs = Utils.multiplyMatrix(this.wih, inputs);
+
+                double[] hiddenOutputs = Arrays.stream(hiddenInputs).map(Utils::activationFunction).toArray();
+
+                double[] finalInputs = Utils.multiplyMatrix(this.who, hiddenOutputs);
+
+                double[] finalOutputs = Arrays.stream(finalInputs).map(Utils::activationFunction).toArray();
+
+                double[] outputErrors = Utils.minusMatrix(target, finalOutputs);
+
+                double[] hiddenErrors = Utils.multiplyMatrix(Utils.transposeMatrix(who), outputErrors);
+
+                this.who = evaluateWeights(outputErrors, hiddenOutputs, finalOutputs);
+                this.wih = evaluateWeights(hiddenErrors, inputs, hiddenOutputs);
+
+                //System.out.println("who = " + Arrays.deepToString(who));
+                //System.out.println("wih = " + Arrays.deepToString(wih));
+        }
+
+        private double[][] evaluateWeights(double[] errors, double[] previousOutputs, double[] afterOutputs) {
+
+
+                double[] doubles = Utils.elementwiseMultiplication(errors, afterOutputs, Utils.oneMinusMatrix(afterOutputs));
+
+
+                double[][] doubles1 = Utils.multiplyMatrix(doubles, previousOutputs);
+
+                double[][] evalOutWeights = Utils.scalarMultiplication(doubles1, lr);
+
+
+                return Utils.sumMatrix(who, evalOutWeights);
+        }
+
+
+}
---
+++ src/com/company/Utils.java
@@ -0,0 +1,109 @@
+package com.company;
+
+public class Utils {
+
+        public static double[] multiplyMatrix(double[][] m1, double[] m2) {
+                int m1ColLength = m1[0].length; // m1 columns length
+                int m2RowLength = m2.length;    // m2 rows length
+                if(m1ColLength != m2RowLength) return null; // matrix multiplication is not possible
+                int mRRowLength = m1.length;    // m result rows length
+                double[] mResult = new double[mRRowLength];
+                for(int i = 0; i < mRRowLength; i++) {         // rows from m1
+//                        for(int j = 0; j < mRColLength; j++) {     // columns from m2
+                                for(int k = 0; k < m1ColLength; k++) { // columns from m1
+                                        mResult[i] += m1[i][k] * m2[k];
+                                }
+//                        }
+                }
+                return mResult;
+        }
+
+        public static double[][] multiplyMatrix(double[] m1, double[] m2) {
+
+                if(m1.length != m2.length)
+                        throw new RuntimeException("could not multiply m1 = " + m1 + "m2 " + m2);
+
+                double[][] result = new double[m1.length][m2.length];
+                int outI = 0, outK = 0;
+                for(int i = 0; i < m1.length; i++) {
+                        for(int k = 0; k < m2.length; k++) {
+                                result[outI][outK++] = m1[i] * m2[k];
+                        }
+                        outI++;
+                        outK = 0;
+                }
+                return result;
+        }
+
+
+        public static double activationFunction(double value){
+                return 1/(1 + Math.pow(Math.E, -1 * value));
+        }
+
+        public static double[] minusMatrix(double[] m1, double[] m2){
+                if (m1.length != m2.length)
+                        throw new RuntimeException("m1 length is " + m1.length + " m2 length is " + m2.length);
+
+                double[] result = new double[m1.length];
+                for (int i = 0; i < m1.length; i++) {
+                        result[i] = m1[i] - m2[i];
+                }
+                return result;
+        }
+
+        public static double[][] transposeMatrix(double[][] matrix){
+
+                double[][] result = new double[matrix.length][matrix[0].length];
+
+                for (int i = 0; i < matrix.length; i++) {
+                        for (int k = 0; k < matrix[0].length; k++) {
+                                result[k][i] = matrix[i][k];
+                        }
+                }
+                return result;
+        }
+
+        public static double[] oneMinusMatrix(double[] matrix){
+
+                double[] result = new double[matrix.length];
+
+                for (int i = 0; i < matrix.length; i++) {
+                      result[i] = 1 - matrix[i];
+                }
+                return result;
+        }
+
+        public static double[] elementwiseMultiplication(double[] m1, double[] m2, double[] m3){
+
+                double [] result = new double[m1.length];
+                for (int i = 0; i < m1.length; i++) {
+                        result[i] = m1[i] * m2[i] * m3[i];
+                }
+                return result;
+        }
+
+
+        public static double[][] scalarMultiplication(double[][] matrix, double scalar){
+
+                double [][] result = new double[matrix.length][matrix[0].length];
+                for (int i = 0; i < matrix.length; i++) {
+                        for (int k = 0; k < matrix[0].length; k++) {
+                                result[i][k] = matrix[i][k] * scalar;
+                        }
+                }
+                return result;
+        }
+
+        public static double[][] sumMatrix(double[][] matrix1, double[][] matrix2){
+
+                double [][] result = new double[matrix1.length][matrix1[0].length];
+                for (int i = 0; i < matrix1.length; i++) {
+                        for (int k = 0; k < matrix1[0].length; k++) {
+                                result[i][k] = matrix1[i][k] + matrix2[i][k];
+                        }
+                }
+                return result;
+        }
+
+
+}
